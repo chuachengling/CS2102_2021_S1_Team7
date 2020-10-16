50cc47e7e31f75dbf2641851d9f08b26f9444ef4
ariadn3 <threadofethariadne@gmail.com>
2020-10-16 21:28:21 +0800

    Fix initializer filepath issue

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -29,7 +29,7 @@ db.init_app(app)
 login_manager.init_app(app)
 
 with app.app_context():
-	initFile = open('sql/init.sql', 'r')
+	initFile = open('FlaskApp/sql/init.sql', 'r')
 	db.session.execute(''.join(line.strip() for line in initFile.readlines()))
 	db.session.commit()
 	initFile.close()
