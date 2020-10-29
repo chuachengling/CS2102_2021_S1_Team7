a4807630174b71b820a6bb19d09f929916934cd3
ariadn3 <threadofethariadne@gmail.com>
2020-10-29 23:12:14 +0800

    Revert app.py local testing change

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,20 +6,27 @@ from views import view
 
 app = Flask(__name__)
 
+# Local test variables
+_LOCAL_TESTING = False
+_USERNAME = "administrator"
+_PASSWORD = "password"
+_HOST = "localhost"
+_PORT = 5432
+_DATABASE = "pcs_application"
+
 # Routing
 app.register_blueprint(view)
-
 
 # Config
 database_url = os.environ.get("DATABASE_URL", None)
 if not database_url:
 	database_url = "postgresql://{username}:{password}@{host}:{port}/{database}"\
     .format(
-        username="administrator",
-        password="password",
-        host="localhost",
-        port=5432,
-        database="pcs_application"
+        username=_USERNAME,
+        password=_PASSWORD,
+        host=_HOST,
+        port=_PORT,
+        database=_DATABASE
     )
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SECRET_KEY"] = "A random key to use flask extensions that require encryption"
@@ -29,7 +36,7 @@ db.init_app(app)
 login_manager.init_app(app)
 
 with app.app_context():
-	initFile = open('sql/init.sql', 'r')
+	initFile = open(('FlaskApp/' if _LOCAL_TESTING else '') + 'sql/init.sql', 'r')
 	db.session.execute(''.join(line.split('--')[0].strip() for line in initFile.readlines()))
 	db.session.commit()
 	initFile.close()
