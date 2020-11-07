9c881ba241dd81dc07cd8c33108ba7df429fc77a
tristancz <62636995+tristancz@users.noreply.github.com>
2020-11-07 20:15:58 +0800

    move ENDIF to correct line

--- FlaskApp/sql/insertFunctions.sql
+++ FlaskApp/sql/insertFunctions.sql
@@ -685,13 +685,13 @@ CREATE OR REPLACE FUNCTION trigger_pending_check()
 RETURNS TRIGGER AS
 $$ BEGIN
 -- Reject all other pending bids in an overlapping period, if an 'Accepted' status is updated/inserted for a specific pet
-  END IF;
   IF NEW.status = 'Accepted' THEN
     UPDATE Looking_After la
     SET status = 'Rejected'
     WHERE la.po_userid = NEW.po_userid AND la.pet_name = NEW.pet_name AND la.status = 'Pending'
     AND NOT (la.start_date < NEW.start_date AND la.end_date < NEW.start_date)
     AND NOT (la.start_date > NEW.end_date AND la.end_date > NEW.end_date);
+  END IF;
   RETURN NULL;
 END;
 $$
